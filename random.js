12abd84abbe921e943db1bdc082194fbc8397558
karlosjota <cj.gomlop@gmail.com>
2015-07-11 13:47:34 +0200

    cambios de prueba

---
+++ random.js
@@ -0,0 +1,8 @@
+// Math.random() devielve número aleatorio ente 0 y 1
+var numero = Math.random();
+
+if (numero <= 0.5) {
+  console.log('\n' + numero + ' Menor que 0.5 \n');
+} else {
+  console.log('\n' + numero + ' Mayor que 0.5 \n');
+}
